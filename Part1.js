0d4f5ec57b67607825c7052ad5dbda618da38b78
Szymek <szymon.rzadca@gmail.com>
2018-07-30 17:53:54 +0200

    some global variables and function that makes use of it

--- Part1.js
+++ Part1.js
@@ -18,3 +18,12 @@ function timesFive(numInput) {
   var result = numInput * 5
   return result
 }
+
+var v = "GLOBAL V"
+var stuff = "GLOBAL STUFF"
+
+function fun(stuff) {
+  console.log(v);
+  stuff = "Reassign stuff inside func"
+  console.log(stuff);
+}
